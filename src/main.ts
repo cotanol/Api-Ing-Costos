06d1bcc5f354830b61012296c75bde55266293f4
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-07-25 13:48:15 -0500

    waaa

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// Importa el módulo principal de la aplicación
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api'); // Establece el prefijo global para todas las rutas
+  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,7 +15,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:3000', // El origen del front
+    origin: 'http://localhost:3000', // TODO: frontend URL, seria mejor usar una variable de entorno
   });
 
   await app.listen(process.env.PORT ?? 8080);
